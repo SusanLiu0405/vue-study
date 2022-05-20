0b18124c3310d5bbf80d7633154ed2aa01512d2e
SusanLiu0405 <15301609750@163.com>
2022-05-20 17:37:21 +0800

    08

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Radio } from 'element-ui';
+import { Button, Radio, Container, Main, Header, Aside } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 
@@ -8,6 +8,10 @@ import router from '../router'
 Vue.config.productionTip = false
 Vue.use(Button);
 Vue.use(Radio);
+Vue.use(Container);
+Vue.use(Main);
+Vue.use(Header);
+Vue.use(Aside);
 
 new Vue({
   router,
