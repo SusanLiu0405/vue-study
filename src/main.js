af52fb048ff3442f5d94f165e7015cc6b6a208ac
SusanLiu0405 <15301609750@163.com>
2022-05-20 18:23:05 +0800

    09

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Radio, Container, Main, Header, Aside } from 'element-ui';
+import { Button, Radio, Container, Main, Header, Aside, Menu, Submenu, MenuItem, MenuItemGroup } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 
@@ -12,6 +12,10 @@ Vue.use(Container);
 Vue.use(Main);
 Vue.use(Header);
 Vue.use(Aside);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(MenuItemGroup);
 
 new Vue({
   router,
